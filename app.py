b14b9878f49e09f6f123b309f7b85b0b44b2b29e
AzizCode92 <azizbenothman76@gmail.com>
2020-12-01 21:51:34 +0100

    add files

--- app.py
+++ app.py
@@ -1,3 +1,37 @@
 """
 app.py
 """
+
+from flask import Flask, render_template, url_for, request, redirect
+from flask_bootstrap import Bootstrap
+
+import os
+import model
+
+app = Flask(__name__, template_folder='template')
+Bootstrap(app)
+
+"""
+Routes
+"""
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.join('images', uploaded_file.filename)
+            print(image_path)
+            uploaded_file.save(image_path)
+            class_name = model.get_prediction(image_path)
+            result = {
+                'class_name': class_name,
+                'image_path': image_path,
+            }
+            return render_template('result.html', result=result)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
